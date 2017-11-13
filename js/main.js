15c8b1f878e55199fb6f7ec209772ab774c43ec0
Tony <ANCLIST@GMAIL.COM>
2017-11-13 14:01:46 -0500

    Add event listener on the button and if it's on change clases to turn it off and if it's off change clases to turn it back on

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  var message = document.querySelector('.status'),
+      button = document.querySelector('.switch'),
+      room = document.querySelector('.light'),
+      onState = 'on',
+      offState = 'off';
 
 
+      button.addEventListener('click', function(e) {
+        if (button.classList.contains(onState)) {
+          room.className = "dark";
+          button.classList.replace( onState, offState );
+          message.innerText = 'It\'s quite dark in here';
+        } else {
+          room.className = "light";
+          button.classList.replace( offState, onState );
+          message.innerText = 'It\'s so bright in here!';
+        };
+      });
+});
